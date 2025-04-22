a0be4a3e42e850adb12313d1c67bbb31126d6286
ashwin <ashwinbose6@gmail.com>
2025-04-22 08:46:04 +0100

    fix: fixed import issues in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GamesModule } from './games/games.module';
 import { LiveGameModule } from './live-game/live-game.module';
 
 @Module({
-  imports: [GamesModule, LiveGameModule],
+  imports: [LiveGameModule],
   controllers: [AppController],
   providers: [AppService],
 })
