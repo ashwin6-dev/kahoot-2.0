3e76fdc01eb8d59e6ff2f09a3bfde76016e3f87e
ashwin <ashwinbose6@gmail.com>
2025-05-07 17:41:39 +0100

    feat: styling applied on games controller

--- backend/src/games/games.controller.ts
+++ backend/src/games/games.controller.ts
@@ -1,19 +1,27 @@
-import { Controller, Post, Get, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 
 @Controller('games')
 export class GamesController {
   @Post()
-  createGame(@Body("questions") questions: number[]) {}
+  createGame(@Body('questions') questions: number[]) {}
 
-  @Get(":id")
+  @Get(':id')
   getGameState(@Param('id', ParseIntPipe) id: number) {}
 
-  @Get(":id/players/:name")
+  @Get(':id/players/:name')
   getPlayerInfo(
-    @Param("id", ParseIntPipe) gameId: number,
-    @Param("name") playerName: string,
+    @Param('id', ParseIntPipe) gameId: number,
+    @Param('name') playerName: string,
   ) {}
 
-  @Delete(":id")
-  removeGame(@Param("id") id: string) {}
+  @Delete(':id')
+  removeGame(@Param('id') id: string) {}
 }
